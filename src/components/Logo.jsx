0e18ead7f781ec578c40d35d604b92f2fa4082c2
JGP <josegilabertpons@hotmail.com>
2024-01-22 13:36:54 +0100

    Update CSS styles, add checkbox functionality to task list

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ export const Logo = () => {
     <div className="col-md-2  customStyle">
     *** Logo ***
 
-   <h1>Learning</h1> 
+   <h1>Learn</h1> 
    <h1 className="text-danger" >Tube</h1> 
     </div>
   )
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -4,19 +4,52 @@ import { useHistory } from "../HistoryContext";
 export const TaskList = () => {
   const [tasks, setTasks] = useState([]);
 
-  const { loading, historyData, currentIdVideo } = useHistory();
+  const { loading, historyData, setHistoryData, currentIdVideo } = useHistory();
 
   useEffect(() => {
     // Update tasks whenever chapters change
     if (!loading && Array.isArray(historyData) && historyData.length > 0) {
-      const indexCurrent = historyData.findIndex( video => video.id === currentIdVideo )
-      const newTasks = historyData[indexCurrent].chapters;
-      setTasks(newTasks);
-      
+      const indexCurrent = historyData.findIndex(
+        (video) => video.id === currentIdVideo
+      );
+      const tasksCurrentVideo = historyData[indexCurrent].chapters;
+      setTasks(tasksCurrentVideo);
     }
   }, [loading, historyData, tasks, currentIdVideo]);
 
+  const handleCheckboxChange = (taskId) => {
+    const updatedHistoryData = [...historyData]; // Create a copy of the array
 
+    const indexCurrent = updatedHistoryData.findIndex(
+      (video) => video.id === currentIdVideo
+    );
+
+    if (indexCurrent !== -1) {
+      const tasksCurrentVideo = updatedHistoryData[indexCurrent].chapters;
+
+      const indexTask = tasksCurrentVideo.findIndex(
+        (task) => task.id === taskId
+      );
+
+      if (indexTask !== -1) {
+        // Create a copy of the task to update its checked property
+        const updatedTask = {
+          ...tasksCurrentVideo[indexTask],
+          checked: !tasksCurrentVideo[indexTask].checked,
+        };
+
+        // Update the array with the new task
+        tasksCurrentVideo[indexTask] = updatedTask;
+
+        // Update the video object with the modified chapters array
+        updatedHistoryData[indexCurrent].chapters = tasksCurrentVideo;
+
+        // Set the state with the updated data
+        setHistoryData(updatedHistoryData);
+      }
+    }
+    localStorage.setItem("cachedData", JSON.stringify(updatedHistoryData));
+  };
 
   return (
     <div className="col-md-3  customStyle">
@@ -30,10 +63,16 @@ export const TaskList = () => {
           <h3>Learning Tasks:</h3>
           {tasks.map((task) => (
             <div className="form-check" key={task.id}>
-            <input
-                className="form-check-input"
+              <input
+                className={
+                  task.checked
+                    ? "form-check-input checked-input-style"
+                    : "form-check-input"
+                }
                 type="checkbox"
                 value="5"
+                checked={task.checked}
+                onChange={() => handleCheckboxChange(task.id)}
                 id={task.id}
               />
               <a
@@ -41,7 +80,9 @@ export const TaskList = () => {
                 target="myiframe"
                 id={`anchor${task.id}$`}
               >
-                <label>{task.title}</label>
+                <label className={task.checked ? "checked-label-style" : ""}>
+                  {task.title}
+                </label>
               </a>
             </div>
           ))}
