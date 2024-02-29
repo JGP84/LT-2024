121849b9079e61e8fefadd8bddf518512940e203
José Gilabert <56225245+JGP84@users.noreply.github.com>
2024-02-29 23:45:44 +0100

    setup github actions

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: process.env.NODE_ENV === 'production'
+    ? '/LT-2024/'
+    : '/',
   resolve: {
     alias: {
       '@': new URL('src', import.meta.url).pathname,
     },
   }
 });
-
