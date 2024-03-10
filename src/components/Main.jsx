3a162bd617a110f6faff6f8eb24bf04ae4f483e5
José Gilabert <56225245+JGP84@users.noreply.github.com>
2024-03-10 13:50:19 +0100

    refactor Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,66 +3,60 @@ import { useHistory } from "../HistoryContext";
 
 export const Main = () => {
   const { loading, setLoading, historyData, currentIdVideo } = useHistory();
-  const [title, setTitle] = useState(null);
-  const [channel, setChannel] = useState(null);
+  const [videoData, setVideoData] = useState({ title: null, channel: null });
 
-  useEffect(() => {
-
+  const loadData = () => {
     const cachedData = localStorage.getItem("cachedData");
 
     if (cachedData === "[]") {
       setLoading(true);
     }
-   
-    // Update tasks whenever chapters change
-    if (!loading && Array.isArray(historyData) && historyData.length > 0) {
+
+    if (!loading && historyData.length > 0) {
       const indexCurrent = historyData.findIndex(
         (video) => video.id === currentIdVideo
       );
 
       if (indexCurrent !== -1 && historyData[indexCurrent]) {
-        const title = historyData[indexCurrent].title;
-        setTitle(title);
-
-        const channel = historyData[indexCurrent].channel;
-        setChannel(channel);
+        setVideoData({
+          title: historyData[indexCurrent].title,
+          channel: historyData[indexCurrent].channel,
+        });
       } else {
-        setTitle(null); // Reset title if it is not available
-        setChannel(null); // Reset channel if it is not available
+        setVideoData({ title: null, channel: null }); // Reset title and channel if they are not available
       }
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loading, historyData, currentIdVideo]);
+  };
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(loadData, [loading, historyData, currentIdVideo]);
 
   return (
     <>
       <div className="row customStyle p-3">
         {loading ? (
-        <>
-          {/* <h5>Loading...</h5> */}
-          <h5>Please, paste a youtube url for create a learning list.</h5> 
-        </>
-      ) : (
-        <>
-          <h2 className="mb-3">{title}</h2>
+          <>
+            {/* <h5>Loading...</h5> */}
+            <h5>Please, paste a youtube url for create a learning list.</h5>
+          </>
+        ) : (
+          <>
+            <h2 className="mb-3">{videoData.title}</h2>
 
-          <iframe
-            className="img-container"
-            name="myiframe"
-            id="myiframe"
-            width="450"
-            height="350"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
+            <iframe
+              className="img-container"
+              name="myiframe"
+              id="myiframe"
+              width="450"
+              height="350"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
 
-          <h3 className="mt-3">{channel}</h3>
-        </>
-      )}
+            <h3 className="mt-3">{videoData.channel}</h3>
+          </>
+        )}
       </div>
-
-      
     </>
   );
 };
